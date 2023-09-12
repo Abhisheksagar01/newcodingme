0c61227087e2f02433d5f926814f09c2020c9778
Abhisheksagar01 <141650796+Abhisheksagar01@users.noreply.github.com>
2023-09-12 15:44:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "sagarabhishek1399@gmail.com";
-$subject = "Mail From codeconia";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@codeconia.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-    header("Location:thankyou.html");
+<?php 
+if(isset($_POST[''])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $msg=$_POST['message'];
+    $to="sagarabhishek1399@gmail.com";
+    $header="from:$email";
+    if(mail($to,$msg,$header)){
+        $msg1="Message submitted successfully";
+    }
+    
 }
-//redirect
-
-?>
